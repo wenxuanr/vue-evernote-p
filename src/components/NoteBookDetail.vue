80202e4ea95a038f74c7cba90f61e0d375b7d884
wenxuanr <vcr01123@gmail.com>
2018-05-31 17:13:11 +0800

    add router

---
+++ src/components/NoteBookDetail.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="note-detail">
+    <h1>{{msg}} : {{ $route.params.noteId }}</h1>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name:"Login",
+    data(){
+      console.log(this.$route);
+      return {
+        msg: "This is detail about notebook..."
+      }
+    }
+  }
+</script>
+
+
+<style scoped>
+    h1 {
+      color: red;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import NoteBookList from '@/components/NoteBookList'
+import NoteBookDetail from '@/components/NoteBookDetail'
+import TrashDetail from '@/components/TrashDetail'
 
-Vue.use(Router)
+
+
+
+Vue.use(Router);
 
 export default new Router({
   routes: [
@@ -10,6 +17,26 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    {
+      path:'/login',
+      name:'Login',
+      component:Login
+    },
+    {
+      path:'/notebooks',
+      name:"notebook",
+      component: NoteBookList
+    },
+    {
+      path:'/note/:noteId',
+      name:"notebookDetail",
+      component: NoteBookDetail
+    },
+    {
+      path:'/trash/:noteId',
+      name:"trash",
+      component: TrashDetail
+    },
   ]
 })
